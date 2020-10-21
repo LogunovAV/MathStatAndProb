c93b149372160a3d4700f7a78a13b94413736f26
logunov <logunov@protonmail.com>
2020-10-22 00:58:49 +0300

    Init commit

---
+++ MathStat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathStat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
